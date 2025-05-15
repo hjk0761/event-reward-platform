61039cd98dc65517913144e066f36858ecfedfca
hjk0761 <hjk0761@naver.com>
2025-05-16 00:57:40 +0900

    feat: auth service db 연결 및 의존성 추가

--- auth/src/app.module.ts
+++ auth/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://mongo:27017/event-reward-db'),
+    AuthModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
