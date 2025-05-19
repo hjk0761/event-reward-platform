00f54195cc32f6d1133ebbbc54d9b207f4dd590f
hjk0761 <hjk0761@naver.com>
2025-05-20 00:51:36 +0900

    feat: 이벤트 서비스 모듈 추가

--- event/src/app.module.ts
+++ event/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserActionModule } from './user-action/user-action.module';
+import { EventModule } from './event/event.module';
+import { RewardModule } from './reward/reward.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://mongo:27017/event-reward-db'),
+    UserActionModule,
+    EventModule,
+    RewardModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
