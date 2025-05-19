559a1ca34639e66a4807cd195001ad70a11da2c6
hjk0761 <hjk0761@naver.com>
2025-05-20 00:57:54 +0900

    feat: Gateway 서버 컨트롤러 분리

---
+++ gateway/src/controllers/user.controller.ts
@@ -0,0 +1,33 @@
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+import { UserService } from '../services/user.service';
+import { RegisterInfo } from '../dto/register-info.dto';
+import { LoginInfo } from '../dto/login-info.dto';
+import { RolesGuard } from '../auth/roles.guard';
+import { Roles } from '../auth/roles.decorator';
+
+@Controller()
+export class UserController {
+  constructor(
+    private readonly userService: UserService,
+  ) { }
+
+  @Post('/register')
+  register(@Body() registerInfo: RegisterInfo) {
+    return this.userService.register(registerInfo.loginId, registerInfo.password, registerInfo.name, registerInfo.role);
+  }
+
+  @Post('/login')
+  login(@Body() loginInfo: LoginInfo) {
+    return this.userService.login(loginInfo.loginId, loginInfo.password);
+  }
+
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles('ADMIN')
+  @Get('/admin')
+  updateRole() {
+    console.log('controller called: /admin');
+    return
+  }
+}
